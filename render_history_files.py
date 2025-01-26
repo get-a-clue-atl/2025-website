47c0ed7e0d9cde0b437e6215d3f6b353cbc52cc3
Benjamin-Vencill <btvencill@protonmail.com>
2025-01-25 20:03:35 -0500

    Update GAC history pages with new template

--- render_history_files.py
+++ render_history_files.py
@@ -56,23 +56,56 @@ def render_html_document(n: int, html_bodies: List[str], output_dir: str) -> Non
     Create an HTML Document from the html_bodies
     """
     template = """<!DOCTYPE html>
-    <html lang="en">
+    <!DOCTYPE html>
+        <html lang="en">
         <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>GAC History</title>
-            <link rel="stylesheet" href="../styles/gac-history-notecards.css">
-            <link rel="preconnect" href="https://fonts.googleapis.com">
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-            <link href="https://fonts.googleapis.com/css2?family=Averia+Libre:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Special+Elite&display=swap" rel="stylesheet">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Test Page</title>
+        <link rel="stylesheet" href="../styles/site-navigation.css">
+        <link rel="stylesheet" href="../styles/body.css">
+        <link rel="stylesheet" href="../styles/gac-history-notecards.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Averia+Libre:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Homemade+Apple&family=Meddon&family=Special+Elite&display=swap" rel="stylesheet">
         </head>
-        <body>
-            <div class="note-container note-container-first">
-                {noteBodies}
-            </div>
+        <body class="home-page">
+            <div class="container">
+                <header>
+                    <div id="header">
+                        <nav class="site-navigation">
+                            <ul>
+                                <li><a href="../index.html">Home</a></li>
+                                <li><a href="../registration.html">Register</a></li>
+                                <li>
+                                <a href="#">Explore</a>
+                                <ul class="dropdown">
+                                    <li><a href="../registration.html">Register</a></li>
+                                    <li><a href="../faq.html">FAQ</a></li>
+                                    <li><a href="../gac-history.html">History</a></li>
+                                    <li><a href="../secrets.html">Secrets</a></li>
+                                    <li><a href="../contact.html">Contact</a></li>
+                                </ul>
+                                </li>
+                                <li><a href="../contact.html">Contact</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </header>
+                <div class="notecard-container">
+                    <div class="note-container note-container-first">
+                        {noteBodies}
+                    </div>
+                </div>
+                <script src="../scripts/addNotebookPage.js"></script>
+            <footer>
+                <div>Made with <span style="color: #e25555;">&#9829;</span> by Get A Clue Atlanta 2025.</div>
+                <div>Illustrations Courtesty of Brett Helquist.</div>
+            </footer>
         </body>
-        <script src="../scripts/addNotebookPage.js"></script>
+        <script src="../scripts/addMenuDropdown.js"></script>
     </html>
+    
     """
 
     html_body = "\n".join(html_bodies)
